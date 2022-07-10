0c23f5beaa36b90f0d71a522c6a97eb58b9cc533
Yue Zhang <yuezhang@compass.com>
2022-07-10 10:29:32 +0800

    add data persist

--- WhenBored/BoredButton/BoredButtonState.swift
+++ WhenBored/BoredButton/BoredButtonState.swift
@@ -7,14 +7,43 @@
 
 import Foundation
 
+let KSaved_BPM = "KSaved_BPM"
+let KSaved_Meter = "KSaved_Meter"
+
 struct BoredButtonState: Equatable {
     static func == (lhs: BoredButtonState, rhs: BoredButtonState) -> Bool {
         return lhs.count == rhs.count && lhs.bpm == rhs.bpm && lhs.currentIndex == rhs.currentIndex && lhs.isCountDown == rhs.isCountDown
     }
     
     static var startIndex = -4
-    var count = 4
-    var bpm = 60
+    var count: Int {
+        get {
+            let ud = UserDefaults.standard
+            let saved = ud.integer(forKey: KSaved_Meter)
+            return saved == 0 ? 4 : saved
+        }
+        
+        set {
+            let ud = UserDefaults.standard
+            ud.set(newValue, forKey: KSaved_Meter)
+            ud.synchronize()
+        }
+    }
+
+    var bpm: Int {
+        get {
+            let ud = UserDefaults.standard
+            let saved = ud.integer(forKey: KSaved_BPM)
+            return saved == 0 ? 60 : saved
+        }
+        
+        set {
+            let ud = UserDefaults.standard
+            ud.set(newValue, forKey: KSaved_BPM)
+            ud.synchronize()
+        }
+    }
+
     var currentIndex = BoredButtonState.startIndex
     var currentAction: BoredButtonAction = .stop
     var title: String {
