eb132a815ef896473c37296bdd98bbc168cf2f66
Yue Zhang <yuezhang@compass.com>
2022-09-20 22:27:18 +0800

    update project name

--- WhenBored/Helpers/Color+MusicalDistance.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-extension Color {
-    /// Color used to display a musical distance that is imperceptible (aka "in tune").
-    static var imperceptibleMusicalDistance: Color { .green }
-    /// Color used to display a musical distance that is perceptible (aka "out of tune").
-    static var perceptibleMusicalDistance: Color { .red }
-}
