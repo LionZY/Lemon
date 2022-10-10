bdee5043a52a16316124a1e5aa6a20a829e961d7
Yue Zhang <yuezhang@compass.com>
2022-10-11 00:27:46 +0800

    update

--- XMetro/Settings/SettingsScreen.swift
+++ XMetro/Settings/SettingsScreen.swift
@@ -37,6 +37,7 @@ struct SettingsScreen: View {
                                 Text(item.title)
                             }
                         }
+                        .accentColor(.black)
                     }
                 }
             }
